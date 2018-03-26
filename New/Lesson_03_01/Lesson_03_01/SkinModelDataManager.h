cc38498dae577a5d346fd36e02c1b87fcde91012
TakayukiKiyohara <1>
2018-03-26 18:33:50 +0900

    add Lesson_03_00,Lesson_03_01

---
+++ New/Lesson_03_01/Lesson_03_01/SkinModelDataManager.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <map>
+
+/*!
+*@brief	スキンモデルデータマネージャー。。
+*/
+class SkinModelDataManager
+{
+public:
+	/*!
+	*@brief	モデルをロード。
+	*@param[in]	filePath	ファイルパス。
+	*/
+	DirectX::Model* Load(const wchar_t* filePath);
+private:
+	//ファイルパスをキー、DirectXModelのインスタンスを値とするマップ。
+	//辞書みたいなものです。
+	std::map< 
+		std::wstring,					//wstringがキー。 
+		std::unique_ptr<DirectX::Model>	//これが値。
+	> m_directXModelMap;
+};
+
+//g_skinModelDataManagerのextern宣言。
+//extern宣言は実態ではないので注意！
+//コンパイラにどこかにあるから使ってねと教えるもの。
+extern SkinModelDataManager g_skinModelDataManager;
+
